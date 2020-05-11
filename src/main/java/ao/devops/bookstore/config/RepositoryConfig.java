f9813d1a3247484b963a248a2be91731c2244738
adallauriano <laureano.adalberto@gmail.com>
2020-05-11 18:15:57 +0100

    finished the first features for backend

---
+++ src/main/java/ao/devops/bookstore/config/RepositoryConfig.java
@@ -0,0 +1,28 @@
+package ao.devops.bookstore.config;
+
+import ao.devops.bookstore.entity.Book;
+import ao.devops.bookstore.entity.BookCategory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
+
+import javax.persistence.EntityManager;
+import javax.persistence.metamodel.Type;
+import java.net.Proxy;
+
+@Configuration
+public class RepositoryConfig implements RepositoryRestConfigurer {
+
+
+    @Autowired
+    private EntityManager entityManager;
+
+    @Override
+    //expose all entities available
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config) {
+        config.exposeIdsFor(entityManager.getMetamodel().getEntities()
+                .stream().map(Type::getJavaType)
+                .toArray(Class[]::new));
+    }
+}
