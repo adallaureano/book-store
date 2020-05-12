e9b702306924c16b39dc69c9abb92ee8b4f89e7e
adallauriano <laureano.adalberto@gmail.com>
2020-05-12 23:43:04 +0100

    shopcart template and migration for db data

--- src/main/java/ao/devops/bookstore/repository/BookRepository.java
+++ src/main/java/ao/devops/bookstore/repository/BookRepository.java
@@ -4,7 +4,7 @@ import ao.devops.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-@CrossOrigin("http://localhost:4200/")
+@CrossOrigin("*")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
