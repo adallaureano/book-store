78f33dde484c250617679f17dc9b842c6fb51d50
adallauriano <laureano.adalberto@gmail.com>
2020-05-12 14:35:18 +0100

    bootstraping the view for books

--- src/main/java/ao/devops/bookstore/repository/BookRepository.java
+++ src/main/java/ao/devops/bookstore/repository/BookRepository.java
@@ -2,8 +2,9 @@ package ao.devops.bookstore.repository;
 
 import ao.devops.bookstore.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200/")
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
