1d448332edee0178ac1322b7fc851084ce7d9a00
adallauriano <laureano.adalberto@gmail.com>
2020-05-13 12:24:37 +0100

    add feature about dynamic categories and migrate db for error in image

--- src/main/angular-bookstore/src/app/services/book.service.ts
+++ src/main/angular-bookstore/src/app/services/book.service.ts
@@ -3,13 +3,15 @@ import { HttpClient} from '@angular/common/http'
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { Book } from '../common/book';
+import { BookCategory } from '../common/book-category';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BookService {
 
-  private baseUrl = "http://localhost:8080/api/v1/books" ; // to show more content add ?size=100
+  private baseUrl = "http://localhost:8080/api/v1/books"; // to show more content add ?size=100
+  private categoryUrl = "http://localhost:8080/api/v1/book-category";
 
   constructor( private httpClient: HttpClient) { }
 
@@ -19,10 +21,23 @@ export class BookService {
       map(response => response._embedded.books)
     );
   }
+
+  getBookCategories(): Observable<BookCategory[]>{
+    return this.httpClient.get<GetResponseBookCategory>(this.categoryUrl).pipe(
+      map(response => response._embedded.bookCategory)
+    );
+  }
 }
 
 interface GetResponseBooks{
   _embedded: {
     books: Book[];
+  }
+}
+
+
+interface GetResponseBookCategory{
+  _embedded: {
+    bookCategory: BookCategory [];
   }
 }
--- src/main/java/ao/devops/bookstore/repository/BookCategoryRepository.java
+++ src/main/java/ao/devops/bookstore/repository/BookCategoryRepository.java
@@ -3,6 +3,8 @@ package ao.devops.bookstore.repository;
 import ao.devops.bookstore.entity.BookCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
 
 @RepositoryRestResource(collectionResourceRel = "bookCategory", path = "book-category")
 public interface BookCategoryRepository extends JpaRepository<BookCategory, Long> {
